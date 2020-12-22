699f7d9511804b00a59528334400703b8ed207dd
RaphHackCoding <61593571+RaphHackCoding@users.noreply.github.com>
2020-12-22 15:19:13 +0100

    Create Bar.js

---
+++ Bar.js
@@ -0,0 +1,14 @@
+alert("Rentrée dans le palace de l'art")
+window.addEventListener('load', () =>{
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const navs = document.querySelectorAll('.nav');
+
+openBtn.addEventListener('click', () => {
+    navs.forEach(nav => nav.classList.add('visible'));
+});
+
+closeBtn.addEventListener('click', () => {
+    navs.forEach(nav => nav.classList.remove('visible'));
+});
+})
